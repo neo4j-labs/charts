4c1e251e7268128f15e619cb0574113c47ec9c7c
Adam Cowley <github@adamcowley.co.uk>
2020-11-13 16:22:43 +0000

    Updated logos

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,9 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createDriver, Neo4jProvider } from 'use-neo4j';
 import { Driver } from 'neo4j-driver'
-import appLogo from './logo.svg'
 import { version } from 'use-neo4j/package.json'
+import appLogo from './logo.svg'
 import 'semantic-ui-css/semantic.min.css'
-
 import 'tailwindcss/dist/tailwind.css'
 import './index.css';
 import store from './store';
@@ -43,7 +42,7 @@ if ( process.env.REACT_APP_NEO4J_SCHEME ) {
 ReactDOM.render(
   <React.StrictMode>
     <ReduxProvider store={store}>
-      <Neo4jProvider driver={driver} logo={logo()} footer={footer()}>
+      <Neo4jProvider logo={logo()} footer={footer()}>
         <App />
       </Neo4jProvider>
     </ReduxProvider>
