2b85727f74eb9ca904b3a1511eac265069a80012
Adam Cowley <github@adamcowley.co.uk>
2021-02-17 17:21:07 +0000

    Fixed scrolling issue in modal windows on screens with a smaller height

--- release-notes.md
+++ release-notes.md
@@ -1,3 +1,7 @@
+## 0.1.7
+
+- Fixed scrolling issue in modal windows on screens with a smaller height
+
 ## 0.1.5
 
 - Added better error handling when the driver can't find the keys that it's looking for from the query results
--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -13,7 +13,7 @@ export default function Modal(props: ModalProps) {
             <div className="modal-underlay fixed inset-0 bg-gray-400 bg-opacity-75" onClick={props.onClose} />
 
             <div className="flex flex-row justify-center z-20">
-                <div className="bg-white rounded-lg shadow-lg" style={{minWidth: '480px'}}>
+                <div className="bg-white rounded-lg shadow-lg overflow-y-auto" style={{minWidth: '480px', maxHeight: '90vh'}}>
                     <div className="flex flex-row justify-between m-4 mb-0 pb-2 text-gray-600 font-bold border-b border-gray-400">
                         {props.title && <h2>{props.title}</h2>}
 
